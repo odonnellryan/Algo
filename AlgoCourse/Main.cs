b41d7d83db433cca8e1469c04fe2af6ffddf137a
odonnellryanc <odonnellryanc@gmail.com>
2014-10-31 14:29:32 -0400

    working quicksort

---
+++ AlgoCourse/Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoCourse
+{
+    class AlgoCourse
+    {
+        static int Main(string[] args)
+        {
+            //... 
+            return 0;
+        }
+    }
+    
+}
---
+++ Tests/TestCases.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace AlgoCourse
+{
+    [TestClass]
+    public class Testing
+    {
+        [TestMethod]
+        public void QuicksortTest()
+        {
+            string path = @"QuickSortFullTestCase.txt";
+            string[] read = File.ReadAllLines(path);
+            List<int> unsortedList;
+            long comparisons;
+
+            // needs to pass test cases: 
+            // size     first    last    median
+            // 10       25       29      21
+            // 100      615      587     518
+            // 1000     10297    10184   8921
+
+            unsortedList = read.Select(int.Parse).ToList();
+            comparisons = new long();
+            Quicksort.Sort(ref unsortedList, 0, unsortedList.Count - 1, ref comparisons, "left");
+            //Assert.AreEqual(10297, comparisons);
+            Console.WriteLine(comparisons);
+
+            unsortedList = read.Select(int.Parse).ToList();
+            comparisons = new long();
+            Quicksort.Sort(ref unsortedList, 0, unsortedList.Count - 1, ref comparisons, "right");
+            //Assert.AreEqual(29, comparisons);
+            Console.WriteLine(comparisons);
+
+            unsortedList = read.Select(int.Parse).ToList();
+            comparisons = new long();
+            Quicksort.Sort(ref unsortedList, 0, unsortedList.Count - 1, ref comparisons, "median");
+            //Assert.AreEqual(8921, comparisons);
+            Console.WriteLine(comparisons);
+        }
+
+        [TestMethod]
+        public void MergeSortTest()
+        {
+            string path = @"MergeSortFullTestCase.txt";
+            string[] read = File.ReadAllLines(path);
+
+            List<int> unsortedList = read.Select(int.Parse).ToList();
+
+            long inversions = new long();
+
+            MergeSort.Sort(unsortedList, ref inversions);
+
+            // print the output - this is for an exam, we don't have anything to compare against.
+            Console.WriteLine(inversions);
+
+        }
+    }
+}
